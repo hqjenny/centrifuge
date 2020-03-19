d6bca2e095339b57f9fb4ea81512010d570f4724
Nathan Pemberton <nathanp@berkeley.edu>
2020-03-19 17:34:38 -0400

    Example vadd project now compiles with libcf and works on x86 in baseline mode. Not tested on real HW yet.

--- examples/vadd_proj/src/benchmark/vadd.c
+++ examples/vadd_proj/src/benchmark/vadd.c
@@ -1,12 +1,10 @@
 #include<stdio.h>
 #include<stdint.h>
 #include<stdlib.h>
+#include <centrifuge.h>
 #define LENGTH 80
 #include "vadd_rocc.h"
 #include "vadd_tl.h"
-
-// Manually calculated reference output
-int golden_output[LENGTH + 1] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79};
 
 void print_vec(int* vec, int length){
     for(int i = 0; i < length; i++){
@@ -47,27 +45,58 @@ int test_rocc(int *c, int *a, int *b, int len) {
     return 0;
 }
 
-int test_tl(int *c, int *a, int *b, int len) {
-  vadd_tl(a, b, c, len);
+int test_tl(cf_ctl_t *ctl, cf_buf_t *c, cf_buf_t *a, cf_buf_t *b, int len)
+{
+  cf_vadd_tl(ctl, a, b, c, len);
   return 0;
 }
 
-int main(int argc, char *argv[]) {
-    int a[LENGTH], b[LENGTH];
+int main(int argc, char *argv[])
+{
+    cf_buf_t a, b, tl_out;
+
+    /*=========================================================================
+     * MEMORY ALLOCATION
+     *=======================================================================*/
+#ifdef CF_LINUX
+    // Dynamically allocate memory that is compatible with tilelink.
+    a = cf_malloc(LENGTH);
+    b = cf_malloc(LENGTH);
+    tl_out = cf_malloc(LENGTH);
+#else
+    //Allocate bare-metal buffers on the stack. This only works with
+    //direct-mapped memory (no virtual memory).
+    int abuf[LENGTH], bbuf[LENGTH];
+    int tl_out_buf[LENGTH] = {0};
+
+    a = cf_buf_init((uint8_t*)abuf, LENGTH);
+    b = cf_buf_init((uint8_t*)abuf, LENGTH);
+    tl_out = cf_buf_init((uint8_t*)tl_out_buf, LENGTH);
+#endif //CF_LINUX
+
+    /*=========================================================================
+     * DATA INITIALIZATION
+     *=======================================================================*/
+    // rocc-only memory can be allocated however you please, it respects
+    // virtual memory and has no additional restrictions.
     int rocc_out[LENGTH] = {0};
-    int tl_out[LENGTH] = {0};
+
     int golden_out[LENGTH] = {0};
 
     for(int i = 0; i < LENGTH; i++){
-      a[i] = i;
-      b[i] = i + 5;
+      ((int*)(a.vaddr))[i] = i;
+      ((int*)(b.vaddr))[i] = i + 5;
     }
 
     for(int i = 0; i < LENGTH; i++) {
-      golden_out[i] = a[i] + b[i];
+      golden_out[i] = ((int*)(a.vaddr))[i] + ((int*)(b.vaddr))[i];
     }
 
-    test_rocc(rocc_out, a, b, LENGTH);
+    /*=========================================================================
+     * RoCC TEST
+     *=======================================================================*/
+    // RoCC only needs the virtual address
+    test_rocc(rocc_out, (int*)a.vaddr, (int*)b.vaddr, LENGTH);
     if(compare_vec(rocc_out, golden_out, LENGTH) != -1) {
       printf("Test Failure: rocc output does not match\n");
       printf("Expected:\n");
@@ -77,13 +106,23 @@ int main(int argc, char *argv[]) {
       return EXIT_FAILURE;
     }
       
-    test_tl(tl_out, a, b, LENGTH);
-    if(compare_vec(tl_out, golden_out, LENGTH) != -1) {
+    /*=========================================================================
+     * TL TEST
+     *=======================================================================*/
+    //Initialize the tilelink accelerator, this only needs to be done once per
+    //session and can be re-used for multiple invocations. The CF_VADD_TL_BASE
+    //macro is defined in the generated wrapper, we also manually define it in
+    //our baseline code so that we can easily switch between the two
+    //implementations.
+    cf_ctl_t vadd_ctl = cf_init(CF_VADD_TL_BASE);
+
+    test_tl(&vadd_ctl, &tl_out, &a, &b, LENGTH);
+    if(compare_vec((int*)(tl_out.vaddr), golden_out, LENGTH) != -1) {
       printf("Test Failure: tilelink output does not match\n");
       printf("Expected:\n");
       print_vec(golden_out, LENGTH);
       printf("Got:\n");
-      print_vec(tl_out, LENGTH);
+      print_vec((int*)(tl_out.vaddr), LENGTH);
       return EXIT_FAILURE;
     }
 
