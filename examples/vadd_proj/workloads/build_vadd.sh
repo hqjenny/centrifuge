7442b8a28682e0edecbf147d5c1b71734b0ed51c
Nathan Pemberton <nathanp@berkeley.edu>
2020-04-08 13:34:00 -0400

    Update workloads for baseline vadd_proj

--- examples/vadd_proj/workloads/build_vadd.sh
+++ examples/vadd_proj/workloads/build_vadd.sh
@@ -1,18 +1,5 @@
 #!/bin/bash
 set -e
 
-pushd ../
-
-pushd src/hls/vadd_tl
-make ARCH=riscv
-popd
-
-pushd src/hls/vadd_rocc
-make ARCH=riscv
-popd
-
-pushd src/benchmark
-make ARCH=riscv
-popd
-
-popd
+make -C ../ ARCH=riscv HOST=linux bench
+make -C ../ ARCH=riscv HOST=bare bench
