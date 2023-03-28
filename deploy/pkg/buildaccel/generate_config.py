ae68d83f4571ef645856ebdfdde087b257c5eaf8
James Shi <jamesshi0001@gmail.com>
2023-03-27 18:43:28 -0700

    new fixes

--- deploy/pkg/buildaccel/generate_config.py
+++ deploy/pkg/buildaccel/generate_config.py
@@ -8,11 +8,11 @@ import re
 from .. import util
 
 logger = logging.getLogger(__name__)
-logger.setLevel(logging.DEBUG) 
+logger.setLevel(logging.DEBUG)
 
 def generate_config(accel_conf):
-    logger.info("Generating HLSConfig.scala file ...") 
-    logger.info("Generating HLSFireSimConfig.scala file ...") 
+    logger.info("Generating HLSConfig.scala file ...")
+    logger.info("Generating HLSFireSimConfig.scala file ...")
 
     import_str = ""
     for accel in accel_conf.rocc_accels:
@@ -28,13 +28,13 @@ def generate_config(accel_conf):
 
     for idx, accel in enumerate(accel_conf.rocc_accels):
         d = {'ACCEL': accel.name, 'IDX': idx}
-        rocc_arr.append(rocc_template.substitute(d)) 
+        rocc_arr.append(rocc_template.substitute(d))
     rocc_str = ",\n        ".join(rocc_arr)
 
-    if len(accel_conf.rocc_accels) > 0: 
-        rocc_str += "," 
+    if len(accel_conf.rocc_accels) > 0:
+        rocc_str += ","
 
-    tl_peri_str = "" 
+    tl_peri_str = ""
     tl_trait_str = ""
     tl_trait_imp_str = ""
     for accel in accel_conf.tl_accels:
@@ -46,12 +46,12 @@ def generate_config(accel_conf):
     template_dir = util.getOpt('template-dir')
     config_dict = {
         #'SOC_NAME': accel_conf.accel_name,
-        'HLS_SOC_IMPORT': import_str,  
+        'HLS_SOC_IMPORT': import_str,
         'ROCC_CONFIG': rocc_str,
         'TL_PERIPHERY': tl_peri_str,
         #'TL_PERIPHERY_IMP': tl_peri_imp_str,
     }
-    
+
     template_path = template_dir / 'HLSConfig_scala_template'
     output_path =  accel_conf.chipyard_scala_dir / 'config' / 'HLSConfig.scala'
     util.generate_file(template_path, config_dict, output_path)
@@ -63,10 +63,10 @@ def generate_config(accel_conf):
         'TL_TRAIT_IMP': tl_trait_imp_str,
     }
 
-    template_path = template_dir / 'Top_scala_template'
-    output_path =  accel_conf.chipyard_scala_dir / 'Top.scala'
+    template_path = template_dir / 'DigitalTop_scala_template'
+    output_path =  accel_conf.chipyard_scala_dir / 'DigitalTop.scala'
     util.generate_file(template_path, config_dict, output_path)
-    logger.info("\t\tGenerate Top.scala: {}".format(output_path))
+    logger.info("\t\tGenerate DigitalTop.scala: {}".format(output_path))
 
     d = {}
     template_path = template_dir / 'HLSTargetConfig_scala_template'
