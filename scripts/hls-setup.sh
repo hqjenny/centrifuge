dd92f0748ecf41191d4505ccf48272c5d7b91ab9
hqjenny <hqjenny@hotmail.com>
2023-06-06 22:33:04 -0700

    Update hls-setup.sh
    Remove out-dated dependency and patch

--- scripts/hls-setup.sh
+++ scripts/hls-setup.sh
@@ -12,9 +12,9 @@ export RDIR=$(git rev-parse --show-toplevel)/../../
 #source sourceme-f1-manager.sh
 
 # modify the pk source code to enable pk for custom instructions
-pushd $RDIR/toolchains/riscv-tools/riscv-pk
-git apply $RDIR/tools/centrifuge/patches/riscv-pk.patch
-popd
+# pushd $RDIR/toolchains/riscv-tools/riscv-pk
+# git apply $RDIR/tools/centrifuge/patches/riscv-pk.patch
+# popd
 
 #pushd $RDIR
 #./scripts/build-toolchains.sh
@@ -44,10 +44,10 @@ popd
 #git apply $RDIR/hls/patches/riscv-linux.patch
 #popd
 #
-pushd $CL_DIR/verif/scripts
-git apply $RDIR/tools/centrifuge/patches/XSim_Makefile.patch 
-touch top.vivado.vhd.f
-popd
+# pushd $CL_DIR/verif/scripts
+# git apply $RDIR/tools/centrifuge/patches/XSim_Makefile.patch 
+# touch top.vivado.vhd.f
+# popd
 
 ## Add Linux static memory allocator for the accelorator
 #cp $RDIR/hls/sw/riscv-linux/hls_mmap_static.c  $RDIR/sw/firesim-software/riscv-linux/mm/
