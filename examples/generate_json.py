5928f292c630c0f784890879404bea8fb46714ec
James Shi <jamesshi0001@gmail.com>
2023-09-02 08:49:31 -0700

    Moved examples to chstone_proj and added Makefiles to those directories

--- examples/generate_json.py
+++ examples/generate_json.py
@@ -14,15 +14,23 @@ with open(sys.argv[1] + "/Makefile") as f:
     for line in f:
         line = line.strip()
         if line.startswith("TARGET"):
-            pgm = line.split("=")[-1]
+            pgm = line.split("=")[-1].strip()
         elif line.startswith("FUNC"):
-            func = line.split("=")[-1]
+            func = line.split("=")[-1].strip()
 
 accel_dict = {"pgm": pgm, "func": func}
 if pgm.endswith("_tl"):
     accel_dict["addr"] = "0x30000"
 srcs = [filename for filename in os.listdir(sys.argv[1]) if filename.endswith(".c") or filename.endswith(".cpp") or filename.endswith(".h")]
-accel_dict["srcs"] = srcs
+
+for src in srcs:
+    with open(sys.argv[1] + "/" + src) as f:
+        src_code = f.read()
+        # Reformatting src_code in case "int main" is formatted weirdly
+        src_code = " ".join(src_code.split())
+        if "int main" in src_code:
+            accel_dict["srcs"] = [src]
+            break
 
 if pgm.endswith("_tl"):
     res = {"TL": [accel_dict]}
